af32ec1e93e9705fa05ffdca0b0817c515a3f21a
huza <huzaifahasif3@gmail.com>
2025-08-07 10:45:14 +0700

    update: header, footer, landing

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="bg-gradient-to-r from-purple-700 via-indigo-700 to-blue-700 text-white py-4 px-6 shadow-md">
+        <header className="border-b border-white/25 background-blur-lg text-white py-4 px-6">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
                 {/* logo */}
                 <Link href="" className="text-2xl font-bold uppercase tracking-wide hover:text-yellow-300 transition">
@@ -24,8 +24,8 @@ export default function Header() {
 
                 {/* cta future paling */}
                 <div>
-                    <Link href="/login" className="px-4 py-2 bg-yellow-400 text-purple-900 font-semibold rounded-md hover:bg-yellow-300 transition">
-                        Sign In
+                    <Link href="/login" className="px-4 py-2 border border-white/25 rounded-lg hover:bg-white/10 hover:border-white/30 transition">
+                        Resume
                     </Link>
                 </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function HomePage() {
   return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Antisuki's Portfolio</h1>
+      <p className="text-lg text-gray-700">
+        landing examplez
+      </p>
+    </div>
   );
 }
