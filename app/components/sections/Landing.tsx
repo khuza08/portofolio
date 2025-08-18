b08db5962baabcb388ef0b3b285cc091538f20bd
huza <huzaifahasif3@gmail.com>
2025-08-18 22:09:19 +0700

    change main bg color to black

--- app/components/sections/Landing.tsx
+++ app/components/sections/Landing.tsx
@@ -16,7 +16,7 @@ export function Landing() {
       className="relative min-h-screen w-full flex items-center justify-center p-8"
       style={{
         backgroundImage: `
-          radial-gradient(circle, rgba(17,17,17,1), transparent 100%),
+          radial-gradient(circle, rgba(0,0,0,1), transparent 100%),
           linear-gradient(to right, rgba(255,255,255,0.1) 1px, transparent 1px),
           linear-gradient(to bottom, rgba(255,255,255,0.1) 1px, transparent 1px)
         `,
